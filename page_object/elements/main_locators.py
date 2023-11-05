60631c779b8e9b42c6f62c67e87005acc01a856f
MultNatalja <multnatalja@gmail.com>
2023-11-05 22:02:05 +0200

    homeWork6 main elements page object tests

--- page_object/elements/main_locators.py
+++ page_object/elements/main_locators.py
@@ -6,3 +6,4 @@ from selenium.webdriver.common.by import By
 class MainLocators:
     TITLE_YOUR_STORE = EC.title_contains("Your Store")
     CURRENCY_VALUE = (By.CSS_SELECTOR, "#content > div.row > div:nth-child(1) > div > div.caption > p.price")
+    ELEMENT_SWIPER_CONTAINER = EC.visibility_of_element_located((By.CSS_SELECTOR, ".swiper-container"))
--- page_object/elements/shop_locators.py
+++ page_object/elements/shop_locators.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class ShopLocators:
     DROPDOWN_CURRENCY = (By.CSS_SELECTOR, "#form-currency div button")
     SELECT_CURRENCY = (By.CSS_SELECTOR, "#form-currency div ul li:nth-child(2) button")
+    ELEMENT_INPUT_SEARCH = EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='search']"))
+    ELEMENT_LOGO = EC.visibility_of_element_located((By.CSS_SELECTOR, "#logo"))
+    ELEMENT_CART_BUTTON = EC.visibility_of_element_located((By.CSS_SELECTOR, "#cart button"))
