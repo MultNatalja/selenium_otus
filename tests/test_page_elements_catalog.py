5ae93b0ff4aadcfef83d81e26b1613efbd24f857
MultNatalja <multnatalja@gmail.com>
2023-11-06 20:30:57 +0200

    homeWork6 catalog page object find elements tests

--- tests/test_page_elements_catalog.py
+++ tests/test_page_elements_catalog.py
@@ -1,12 +1,11 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from page_object.catalog_page import CatalogPage
+from page_object.elements.catalog_locators import CatalogLocators
 
 
-def test_find_elements_catalog(browser, url):
-    browser.get(url + "/desktops")
-    WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".product-layout")))
-    WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-limit")))
-    WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".image")))
-    WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-sort")))
-    WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".button-group button")))
+def test_find_elements_catalog(browser):
+    catalog_page = CatalogPage(browser)
+    catalog_page._element(CatalogLocators.PRODUCT_LAYOUT)
+    catalog_page._element(CatalogLocators.INPUT_LIMIT)
+    catalog_page._element(CatalogLocators.IMAGE_CATALOG)
+    catalog_page._element(CatalogLocators.INPUT_SORT)
+    catalog_page._element(CatalogLocators.CART_ADD_BUTTON)
